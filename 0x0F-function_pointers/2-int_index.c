a6b65f23e81200999b15e9fe737aa51de7109945
Yassine <yassinesbi45@gmail.com>
2023-11-08 15:18:26 -0800

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,9 +8,6 @@
  * Return: Index of the first matching element, or -1 if no match.
  */
 int int_index(int *array, int size, int (*cmp)(int)) {
-    if (size <= 0 || array == NULL || cmp == NULL) {
-        return -1;
-    }
 
     int i;
     for (i = 0; i < size; i++) {
